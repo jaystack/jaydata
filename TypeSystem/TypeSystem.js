df4dd0ba7ead5596d84dbeed1f2f06158ae635cb
Zenima <zenima7@gmail.com>
2012-06-05 16:39:09 +0200

    Replaced jQuery.extend() method with a simple version of our own. Added unit tests

--- TypeSystem/TypeSystem.js
+++ TypeSystem/TypeSystem.js
@@ -980,14 +980,33 @@ $data.typeSystem = {
         ctor.prototype.constructor = ctor;
         return ctor;
     },*/
-    mix: function (type, mixin) {
-        type.prototype = $.extend(type.prototype || {}, mixin.prototype || {});
-        type.mixins = type.mixins || [];
-        type.mixins.push(mixin);
-        return type;
-    },
-    extend: function (target, obj1) {
-        return $.extend(target, obj1);
+    //mix: function (type, mixin) {
+    //    type.prototype = $.extend(type.prototype || {}, mixin.prototype || {});
+    //    type.mixins = type.mixins || [];
+    //    type.mixins.push(mixin);
+    //    return type;
+    //},
+    extend: function (target) {
+        /// <summary>
+        /// Extends an object with properties of additional parameters.
+        /// </summary>
+        /// <signature>
+        /// <param name="target" type="Object">Object that will be extended.</param>
+        /// <param name="object" type="Object">Object to extend target with.</param>
+        /// <param name="objectN" optional="true" parameterArray="true" type="Object">Object to extend target with.</param>
+        /// </signature>        
+    	/// <returns></returns>
+        if (typeof target !== 'object')
+            Guard.raise('Target must be object');
+        for (var i = 1; i < arguments.length; i++) {
+            var obj = arguments[i];
+            if (obj === null || typeof obj === 'undefined')
+                continue;
+            for (key in obj) {
+                target[key] = obj[key];
+            }
+        }        
+        return target;
     },
     createCallbackSetting: function (callBack, defaultSetting) {
         var setting = {
