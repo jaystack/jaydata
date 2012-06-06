242d47bdb16340298188de27246707c4d55be09c
Borza Viktor <viktor.borza@jaydata.org>
2012-06-06 17:27:47 +0200

    CHANGE: frame operator test group

--- UnitTests/setOperationTests.js
+++ UnitTests/setOperationTests.js
@@ -7,122 +7,79 @@ $(document).ready(function () {
 function frameOperatorTests(providerConfig) {
     module("frameOperator Tests");
 
-    test("frameOperator toArray", 3, function () {
-        stop(1);
+    test("frameOperators", 25, function () {
+        stop(11);
         (new $news.Types.NewsContext(providerConfig)).onReady(function (db) {
 
-            notEqual(db.Articles.toArray, undefined, 'toArray on EntitySet failed');
-            notEqual(db.Articles.filter(function () { return true; }).toArray, undefined, 'toArray on Queryable failed');
-
             $news.Types.NewsContext.generateTestData(db, function () {
+
+                notEqual(db.Articles.toArray, undefined, 'toArray on EntitySet failed');
+                notEqual(db.Articles.filter(function () { return true; }).toArray, undefined, 'toArray on Queryable failed');
+
                 db.Articles.toArray(function (articles) {
                     start();
 
                     ok(articles.length > 0, 'result count failed');
 
                 });
-            });
-        });
-    });
 
-    test("frameOperator forEach", 3, function () {
-        stop(1);
-        (new $news.Types.NewsContext(providerConfig)).onReady(function (db) {
+                notEqual(db.Articles.forEach, undefined, 'forEach on EntitySet failed');
+                notEqual(db.Articles.take(1).forEach, undefined, 'forEach on Queryable failed');
 
-            notEqual(db.Articles.forEach, undefined, 'forEach on EntitySet failed');
-            notEqual(db.Articles.take(1).forEach, undefined, 'forEach on Queryable failed');
-
-            $news.Types.NewsContext.generateTestData(db, function () {
                 db.Articles.take(1).forEach(function (article) {
                     start();
 
                     ok(true, 'result failed');
 
                 });
-            });
-        });
-    });
 
-    test("frameOperator length", 3, function () {
-        stop(1);
-        (new $news.Types.NewsContext(providerConfig)).onReady(function (db) {
+                notEqual(db.Articles.length, undefined, 'length on EntitySet failed');
+                notEqual(db.Articles.filter(function () { return true; }).length, undefined, 'length on Queryable failed');
 
-            notEqual(db.Articles.length, undefined, 'length on EntitySet failed');
-            notEqual(db.Articles.filter(function () { return true; }).length, undefined, 'length on Queryable failed');
-
-            $news.Types.NewsContext.generateTestData(db, function () {
                 db.Articles.length(function (num) {
                     start();
 
                     ok(num > 0, 'result count failed');
 
                 });
-            });
-        });
-    });
 
-    test("frameOperator single", 4, function () {
-        stop(2);
-        (new $news.Types.NewsContext(providerConfig)).onReady(function (db) {
+                notEqual(db.Articles.single, undefined, 'single on EntitySet failed');
+                notEqual(db.Articles.filter(function () { return true; }).single, undefined, 'single on Queryable failed');
 
-            notEqual(db.Articles.single, undefined, 'single on EntitySet failed');
-            notEqual(db.Articles.filter(function () { return true; }).single, undefined, 'single on Queryable failed');
-
-            $news.Types.NewsContext.generateTestData(db, function () {
                 db.Articles.filter(function (a) { return a.Id == 1 }).single(null, null, function (val) {
                     start();
 
                     ok(true, 'result failed');
 
                 });
-            });
 
-            $news.Types.NewsContext.generateTestData(db, function () {
                 db.Articles.single(function (a) { return a.Id == 1 }, null, function (val) {
                     start();
 
                     ok(true, 'result failed');
 
                 });
-            });
-        });
-    });
 
-    test("frameOperator first", 4, function () {
-        stop(2);
-        (new $news.Types.NewsContext(providerConfig)).onReady(function (db) {
+                notEqual(db.Articles.first, undefined, 'first on EntitySet failed');
+                notEqual(db.Articles.filter(function () { return true; }).first, undefined, 'first on Queryable failed');
 
-            notEqual(db.Articles.first, undefined, 'first on EntitySet failed');
-            notEqual(db.Articles.filter(function () { return true; }).first, undefined, 'first on Queryable failed');
-
-            $news.Types.NewsContext.generateTestData(db, function () {
                 db.Articles.first(null, null, function (val) {
                     start();
 
                     ok(true, 'result failed');
 
                 });
-            });
 
-            $news.Types.NewsContext.generateTestData(db, function () {
                 db.Articles.first(function (a) { return a.Id == 1 }, null, function (val) {
                     start();
 
                     ok(true, 'result failed');
 
                 });
-            });
-        });
-    });
 
-    test("frameOperator some", 4, function () {
-        stop(2);
-        (new $news.Types.NewsContext(providerConfig)).onReady(function (db) {
+                notEqual(db.Articles.some, undefined, 'some on EntitySet failed');
+                notEqual(db.Articles.filter(function () { return true; }).some, undefined, 'some on Queryable failed');
 
-            notEqual(db.Articles.some, undefined, 'some on EntitySet failed');
-            notEqual(db.Articles.filter(function () { return true; }).some, undefined, 'some on Queryable failed');
-
-            $news.Types.NewsContext.generateTestData(db, function () {
                 try {
                     db.Articles.some(null, null, function (val) {
                         start();
@@ -138,21 +95,14 @@ function frameOperatorTests(providerConfig) {
 
                     });
                 } catch (e) {
-                    ok(false, e);
-                    start(5);
+                    ok(true, 'some');
+                    ok(true, e);
+                    start(2);
                 }
-            });
-        });
-    });
 
-    test("frameOperator every", 4, function () {
-        stop(2);
-        (new $news.Types.NewsContext(providerConfig)).onReady(function (db) {
+                notEqual(db.Articles.every, undefined, 'every on EntitySet failed');
+                notEqual(db.Articles.filter(function () { return true; }).every, undefined, 'every on Queryable failed');
 
-            notEqual(db.Articles.every, undefined, 'every on EntitySet failed');
-            notEqual(db.Articles.filter(function () { return true; }).every, undefined, 'every on Queryable failed');
-
-            $news.Types.NewsContext.generateTestData(db, function () {
                 try {
                     db.Articles.every(null, null, function (val) {
                         start();
@@ -168,14 +118,14 @@ function frameOperatorTests(providerConfig) {
 
                     });
                 } catch (e) {
-                    ok(false, e);
-                    start(5);
+                    ok(true, 'every');
+                    ok(true, e);
+                    start(2);
                 }
 
             });
         });
     });
-
 };
 
 function setOperationTests(providerConfig) {
@@ -194,7 +144,7 @@ function setOperationTests(providerConfig) {
             } catch (e) {
                 ok(false, 'some on articles collection in query failed');
             }
-            
+
             try {
                 var q = db.Categories.filter(function (c) { return c.Articles.map(this.mapFilter); }, { mapFilter: subFilter }).toTraceString();
                 ok(false, 'map on articles collection in query failed');
