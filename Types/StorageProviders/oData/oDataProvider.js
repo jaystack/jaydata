782b776639f5f0083acf99256409dbfd136711df
Zenima <zenima7@gmail.com>
2012-06-07 14:11:42 +0200

    Fixed a potential bug which caused invalid this arguments in user defined beforeSend requests in oData. Commented out metadata generation related methods because their functionallity is now implemented in JaySvcUtil

--- Types/StorageProviders/oData/oDataProvider.js
+++ Types/StorageProviders/oData/oDataProvider.js
@@ -24,8 +24,6 @@ $C('$data.storageProviders.oData.oDataProvider', $data.StorageProviderBase, null
             case $data.storageProviders.sqLite.DbCreationType.DropAllExistingTables:
                 var that = this;
                 if (this.providerConfiguration.serviceUrl) {
-                    
-                    
                     $data.ajax(this._setAjaxAuthHeader({
                         url: that.providerConfiguration.serviceUrl + "/Delete",
                         type: 'POST',
@@ -92,14 +90,14 @@ $C('$data.storageProviders.oData.oDataProvider', $data.StorageProviderBase, null
             var user = this.providerConfiguration.user;
             var password = this.providerConfiguration.password;
             var origBeforeSend = cfg.beforeSend;
-            cfg.beforeSend = function(xhr) {
+            cfg.beforeSend = function (xhr) {
                 xhr.setRequestHeader("Authorization", "Basic " + encodeBase64(user + ":" + password || ""));
                 if (typeof origBeforeSend === "function")
-                    origBeforeSend(xhr);
+                    origBeforeSend.apply(this, xhr);
             }
         }
         return cfg;
-    },    
+    },
     //buildDbType_modifyInstanceDefinition: function (instanceDefinition, storageModel) {
     //    if (storageModel.Associations) {
     //        storageModel.Associations.forEach(function (association) {
@@ -177,8 +175,8 @@ $C('$data.storageProviders.oData.oDataProvider', $data.StorageProviderBase, null
             }
         };
 
-        this.context.prepareRequest.call(this, requestData);
-        $data.ajax(this._setAjaxAuthHeader(requestData));
+        this.context.prepareRequest.call(this, this._setAjaxAuthHeader(requestData));
+        $data.ajax(requestData);
     },
     _compile: function (queryable, params) {
         var compiler = new $data.storageProviders.oData.oDataCompiler();
@@ -470,7 +468,7 @@ $C('$data.storageProviders.oData.oDataProvider', $data.StorageProviderBase, null
         }
         return keyValue;
     },
-
+    /*
     getServiceMetadata: function () {
         $data.ajax(this._setAjaxAuthHeader({
             url: this.providerConfiguration.oDataServiceHost + "/$metadata",
@@ -506,6 +504,7 @@ $C('$data.storageProviders.oData.oDataProvider', $data.StorageProviderBase, null
                 break;
         }
     }
+    */
 }, null);
 
 $data.StorageProviderBase.registerProvider("oData", $data.storageProviders.oData.oDataProvider);
