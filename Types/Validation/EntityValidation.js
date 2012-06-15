5bc90f62ab085176f16b8dacfa3180abfa3d49d0
Borza Viktor <viktor.borza@jaydata.org>
2012-06-15 14:44:25 +0200

    FIX: entityValidation partial update fix

--- Types/Validation/EntityValidation.js
+++ Types/Validation/EntityValidation.js
@@ -16,7 +16,7 @@ $data.Class.define('$data.Validation.EntityValidation', $data.Validation.EntityV
         var errors = [];
         var typeName = Container.resolveName(Container.resolveType(memberDefinition.dataType));
         var value = !valueNotSet ? newValue : entity[memberDefinition.name];
-        this.fieldValidate(memberDefinition, value, errors, typeName);
+        this.fieldValidate(entity, memberDefinition, value, errors, typeName);
         return errors;
     },
 
@@ -83,17 +83,19 @@ $data.Class.define('$data.Validation.EntityValidation', $data.Validation.EntityV
         }
     },
 
-    fieldValidate: function (memberDefinition, value, errors, validationTypeName) {
+    fieldValidate: function (entity, memberDefinition, value, errors, validationTypeName) {
         ///<param name="memberDefinition" type="$data.MemberDefinition" />
         ///<param name="value" type="Object" />
         ///<param name="errors" type="Array" />
         ///<param name="validationTypeName" type="string" />
+        if ((entity.entityState == $data.EntityState.Modified && entity[memberDefinition.name] == undefined))
+            return;
 
         var validatonGroup = this.supportedValidations[validationTypeName];
         if (validatonGroup) {
             var validations = Object.keys(validatonGroup);
             validations.forEach(function (validation) {
-                if (memberDefinition[validation] && validatonGroup[validation] && !validatonGroup[validation](value, this.getValidationValue(memberDefinition, validation)))
+                if (memberDefinition[validation] && validatonGroup[validation] && !validatonGroup[validation].call(entity, value, this.getValidationValue(memberDefinition, validation)))
                     errors.push(this.createValidationError(memberDefinition, validation, 'Validation error!'));
             }, this);
         }
