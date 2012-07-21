cf23c90a5767f1b840d4699c033b56073924995d
PeterAronZentai <moswes22>
2012-07-21 09:02:00 +0200

    NodeJS based test webserver to run tests

---
+++ webserver/localserver.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: zpace
+ * Date: 7/19/12
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var c = require('connect');
+
+var app = c();
+
+app.use("/", c["static"]("../"));
+app.listen(8888);
