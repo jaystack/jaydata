0b0d33bcc8562613f6b7fde8cf162df5d4e70eaf
Borza Viktor <viktor.borza@jaydata.org>
2012-08-08 13:55:18 +0200

    test fle fix

--- nodeJsTest.js
+++ nodeJsTest.js
@@ -4,9 +4,9 @@ require('./NewsReaderContext.js');
 var reporter = require('nodeunit').reporters.default;
 
 //Tests
-//exports['EntityTransform'] = require('./UnitTests/NodeJS/EntityTransformTests.js');
-//exports['oDataMetaDataGenerator'] = require('./UnitTests/NodeJS/oDataMetaDataGeneratorTests.js');
-//exports['oDataResponseDataBuilder'] = require('./UnitTests/NodeJS/oDataResponseDataBuilderTests.js');
+exports['EntityTransform'] = require('./UnitTests/NodeJS/EntityTransformTests.js');
+exports['oDataMetaDataGenerator'] = require('./UnitTests/NodeJS/oDataMetaDataGeneratorTests.js');
+exports['oDataResponseDataBuilder'] = require('./UnitTests/NodeJS/oDataResponseDataBuilderTests.js');
 
 exports['oDataBatchTest'] = require('./UnitTests/NodeJS/oDataBatchTests.js');
 
