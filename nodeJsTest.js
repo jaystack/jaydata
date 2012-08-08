946646da37b872fca18929b5125c7149923a11c4
Borza Viktor <viktor.borza@jaydata.org>
2012-08-08 09:27:51 +0200

    fix: nodejs test

--- nodeJsTest.js
+++ nodeJsTest.js
@@ -5,7 +5,7 @@ var reporter = require('nodeunit').reporters.default;
 
 //Tests
 exports['EntityTransform'] = require('./UnitTests/NodeJS/EntityTransformTests.js');
-//exports['oDataMetaDataGenerator'] = require('./UnitTests/NodeJS/oDataMetaDataGeneratorTests.js');
-//exports['oDataResponseDataBuilder'] = require('./UnitTests/NodeJS/oDataResponseDataBuilderTests.js');
+exports['oDataMetaDataGenerator'] = require('./UnitTests/NodeJS/oDataMetaDataGeneratorTests.js');
+exports['oDataResponseDataBuilder'] = require('./UnitTests/NodeJS/oDataResponseDataBuilderTests.js');
 
 reporter.run(exports);
