6375cd7aa435affbef3ab5f1ef9819bdf5f0d059
Peter Aron Zentai <peter.zentai@avangrade.com>
2012-08-08 12:35:01 +0200

    JayServices client test

--- JayService/app.js
+++ JayService/app.js
@@ -1,69 +1,72 @@
-/**
- * Created with JetBrains WebStorm.
- * User: zpace
- * Date: 7/6/12
- * Time: 8:00 PM
- * To change this template use File | Settings | File Templates.
- */
-require('jaydata');//["$data"];
 
+require('jaydata');
+var connect = require('connect');
+
+//annotate with JSON Schema
 $data.Entity.extend("Demo.Person", {
-    FirstName:{ type:"string" },
-    LastName:{type:"string" },
-    Age:{ type:"number" }
+    FirstName: { type:"string", required: true },
+    LastName: {type:"string" , required: true},
+    Age: { type:"number", required: true, minValue: 1, maxValue: 150 }
 });
 
-$data.ServiceBase.extend("Demo.Service", {
+//annotate with JSON Schema
+$data.Entity.extend("Demo.Order", {
+    OrderDate: { type:"date", required: true },
+    PersonName: {type:"string" , required: true},
+    Amount: { type:"number", required: true }
+});
 
-    MyFunkyFunction:$data.JayService.serviceFunction()
-        .param("a", "number")
-        .returnsArrayOf("number")
-        (function (a) {
-            return [5, 6, 7, 8, 9];
+$data.ServiceBase.extend("Demo.PersonAPI", {
+
+    //annotate with fluent api
+    createPerson:$data.JayService.serviceFunction()
+        .param("firstName", "string")
+        .param("lastName", "string")
+        .param("age","number")
+        .returns("Demo.Person")
+        (function (firstName, lastName, age) {
+            //call to storage, omitted
+            return new Demo.Person( { FirstName: firstName, LastName: lastName, Age: age})
         }),
 
+    //annotate with comments
+    getPersonsByAge: function (age) {
+            ///<param name="age" type="number" />
+            ///<returns type="Array" elementType="Demo.Person" />
+            return [
+                new Demo.Person({FirstName:"Viktor", LastName:'Borza', Age: age}),
+                new Demo.Person({FirstName:"Viktor", LastName:'Lazar', Age: age})
+            ];
+        },
 
-    SomeFunction: function(a,b,c) {
-        return 42;
-    },
-
-    GetPersonsByAge: $data.JayService.serviceFunction().
-        param("age", "Array").
-        returnsArrayOf("Demo.Person")
-        (function (age) {
-            return [ new Demo.Person({FirstName:"Viktor", LastName:'Borza'}), new Demo.Person({FirstName:"Viktor", LastName:'Lazar'}) ];
-        }),
-
-    MyOtherFunction:function (a, b, c) {
-        ///<param name="a" type="string" />
-        ///<param name="b" type="string" />
-        ///<param name="c" type="number" />
-        ///<returns type="Array" elementType="Object" />
-        return [ new Demo.Person({FirstName:"Viktor", LastName:'Borza'}), new Demo.Person({FirstName:"Viktor", LastName:'Lazar'}) ];
-    },
-
-    MyOtherFunctionX:function (a, b, c) {
-        ///<param name="a" type="string" />
-        ///<param name="b" type="string" />
-        ///<param name="c" type="number" />
-        ///<returns type="Object" />
-        return [a, b, c];
+    constructor: function() {
+        this.internalFunction = function() {
+            //not visible on OData endpoint
+        }
     }
 });
 
-Demo.Service.annotateFromVSDoc();
+Demo.PersonAPI.annotateFromVSDoc();
 
-//var adapter = service.createAdapter(ServiceClass,  instanceFactory);
-var connect = require('connect');
-var app = require('connect')();
+//publish service
+var app = connect();
 app.use(connect.query());
-app.use("/", connect.static("../"));
-//app.use("/ServiceClass", adapter);
-app.use("/x", $data.JayService.createAdapter(Demo.Service, function () {
-    return new Demo.Service
-}));
-//var myAdapter = service.createAdapter($abc.SVC, function() { return new $abc.SVC({name:'Facebook'}) });
-//app.use("/my", myAdapter);
-
+app.use("/personApi", $data.JayService.createAdapter(Demo.PersonAPI));
 app.listen(3001);
 console.log("started");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//app.use("/", connect.static("../"));
+//app.use("/ServiceClass", adapter);
