89d7c6fc3b8e09953089844e740be8692320f03a
Borza Viktor <viktor.borza@jaydata.org>
2012-08-20 17:57:58 +0200

    Modify: fullRoute Calculate

--- JayService/JayService.js
+++ JayService/JayService.js
@@ -37,8 +37,12 @@ $data.Class.define("$data.JayService", null, null, {
 
         return f;
     },
-    createAdapter:function (type, instanceFactory) {
+    createAdapter: function (type, instanceFactory) {
+        var self = this;
         return function (req, res, next) {
+            
+            self.routeParser(this, req);
+
             var factory = instanceFactory || function() { return new type; };
             var adapter = new $data.JSObjectAdapter(type, factory);
             adapter.handleRequest(req, res, next);
@@ -46,6 +50,17 @@ $data.Class.define("$data.JayService", null, null, {
     },
     resultAsXml:function (data) {
         return new $data.XmlResult(data);
+    },
+    routeParser: function (app, req) {
+        var schema = 'http';
+        if (req && req.headers) {
+            if(req.headers['X-Forwarded-Protocol'] === 'https')
+                schema += 's';
+
+            if (req.headers.host) {
+                req.fullRoute = schema + '://' + req.headers.host + app.route;
+            }
+        }
     }
 });
 
