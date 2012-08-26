455954431eecde483ebe3dd0bcf061bf7b25ccd7
Gabor Dolla <agdolla@gmail.com>
2012-08-26 06:49:38 -0700

    provisioning

--- JayService/provision/provision.js
+++ JayService/provision/provision.js
@@ -1,174 +1,185 @@
 
-var q = require('q');
-var exec = require('exec');
-
-var Db = require('mongodb').Db
+var q = require('q')
+    , Db = require('mongodb').Db
     , Admin = require('mongodb').Admin
     , Connection = require('mongodb').Connection
     , Server = require('mongodb').Server
-    , ReplSetServers = require('mongodb').ReplSetServers;
-
-
-var mongoose = module.parent.exports.mongoose
+    , mongoose = module.parent.exports.mongoose
     , Schema = mongoose.Schema
-    , ObjectId = Schema.ObjectId;
+    , ObjectId = Schema.ObjectId
+    , uuid = require('node-uuid');
 
-var extend = require('mongoose-schema-extend');
-
-var tokensrv = module.parent.exports.tokensrv;
-
-var app = module.parent.exports.app;
-
-var AppItemSchema = new Schema({
-    appid: { type: String }
-}, { collection : 'AppItems', discriminatorKey : 'type' });
-
-var CUSchema = AppItemSchema.extend({
-    instanceid: { type: String },
-    name: { type: String },
-    ip: { type: String }
+var InventorySchema = new Schema({
+    instanceid: String, // TODO should be unique key
+    size: String, // micro
+    type: String, // reserved, ondemand, spot
+    used: Boolean,
+    appID: String, // has value if this instance is in use
+    cuID: String, // has value if this instance is in use
+    lastModified: Date
 });
 
-var FileRepoSchema = AppItemSchema.extend({
-    quota: { type: Number }
+var AppSchema = new Schema({
+    appID: String
 });
 
-var QueryableDBSchema = AppItemSchema.extend({
-    dbid: { type: String },
-    dbname: { type: String },
-    maxrecord: { type: Number }
+var ProvisionableAppSchema = new Schema({
+    appID: String,
+    provisionID: String,
+    name: String
 });
 
-var AppItem = mongoose.model('appitem', AppItemSchema);
-var CUItem = mongoose.model('cu', CUSchema);
-var FileRepoItem = mongoose.model('filerepo', FileRepoSchema);
-var QueryableDbItem = mongoose.model('qdb', QueryableDBSchema);
+var AppInstanceSchema = new Schema({
+    appID: String,
+    provisionID: String,
+    parentAppID: String
+});
 
-var types = { 'ComputeUnits': createComputeUnit, 'FileRepo': createFileRepo, 'QueryableDB': createQueryableDB };
+var DbSchema = new Schema({
+    appID: String,
+    dbname: String
+});
 
-function launch(appid) {
-    var tag = 'Name:'+appid;
-    exec(['launch_instance', '-g', 'application', '-a', 'ami-e901069d', '-t', 't1.micro', '-r', 'eu-west-1', '-z', 'eu-west-1b',
-        '-T', tag, '-k', 'jaystack', '-d', '~/.boto'], function(err, out, exitcode) {
-        if (err) throw err;
+var ProvisionedDbSchema = new Schema({
+    appID: String,
+    dbname: String,
+    username: String,
+    password: String
+});
 
-    });
+var InventoryItem = mongoose.model('inventoryitem', InventorySchema);
+var AppItem = mongoose.model('apps', AppSchema);
+var ProvisionableAppItem = mongoose.model('provisionableapps', ProvisionableAppSchema);
+var AppInstanceItem = mongoose.model('appinstances', AppInstanceSchema);
+var DbItem = mongoose.model('dbs', DbSchema);
+var ProvisionedDbItem = mongoose.model('provisioneddbs', ProvisionedDbSchema);
 
-    // Public DNS name: ec2-46-51-157-43.eu-west-1.compute.amazonaws.com
-    // Private DNS name: ip-10-228-89-130.eu-west-1.compute.internal
+function checkProvisionId(appid, provisionid, callback) {
+    ProvisionableAppItem.find( { appID: appid, provisionID: provisionid }, (function (err, items) {
+        if (items.length == 0) callback('not valid provisionid');
+        else callback(err, items);
+    }));
 }
 
-function createComputeUnit(req) {
-
-    var appitem = new CUItem({
-        id: req.body._id,
-        appid: req.body.appid,
-        test: req.body.test
-    });
-    if (appitem.test == false) {
-        //ip = launch(req.body.appid);
-        console.log("launch");
-    }
-
-    return appitem;
+function createNewApp(parentAppID, provisionid, newappid, callback) {
+    var appinstanceitem = new AppInstanceItem( { appID : newappid, provisionID: provisionid, parentAppID: parentAppID } );
+    appinstanceitem.save(callback);
 }
 
-function createFileRepo(req) {
-    var appitem = new FileRepoItem({
-        id: req.body._id,
-        appid: req.body.appid,
-        quota: req.body.quota
+function createDatabases(parentAppID, newappid, username, password, callback) {
+    DbItem.find({appID: parentAppID}, function(err, dbs) {
+        if (err) callback(err);
+        else {
+            var dbz = [];
+            var promise = q.resolve(1); // ?
+            for (var i=0; i<dbs.length; i++) {
+                var dbitem = dbs[i];
+                var provisionedDbItem = new ProvisionedDbItem( {
+                    appID: newappid,
+                    dbname: newappid + '_' + dbitem.dbname,
+                    username: username,
+                    password: password
+                });
+                promise = promise.then(function() { return q.ncall(createQueryableDB, this, provisionedDbItem); } )
+                        .then(function(newdb) { dbz.push( { db: newdb, suffix: dbitem.dbname } ); return newdb; } )
+                        .then(function() { return q.ncall(provisionedDbItem.save, provisionedDbItem); });
+            }
+            promise.then(function() { callback(null, dbz); } )
+                   .fail(function(reason) { callback(reason); } );
+        }
     });
-    return appitem;
 }
 
-function createQueryableDB(req) {
-    var appitem = new QueryableDbItem({
-        id: req.body._id,
-        appid: req.body.appid,
-        dbid: req.body.dbid,
-        dbname: req.body.dbname,
-        maxrecord: req.body.maxrecord
-    });
-    var newdb = new Db('my_database', new Server('db1.storm.jaystack.com', 8888, { auto_reconnect: true }), {});
-    newdb.open(function(err,db) {
-        db.admin(function(err, admindb) {
-            admindb.authenticate('admin', 'admin', function(err, result) {
-                db.addUser('x','y', function(err, result) {
+function createQueryableDB(provisionedDbItem, callback) {
+    var newdb = new Db(provisionedDbItem.dbname, new Server('localhost', 27017, {}));
+    newdb.open(function(err, db) {
+        if (err) callback(err);
+        else db.admin(function(err, admindb) {
+            if (err) callback(err);
+            else admindb.authenticate('admin', 'admin', function(err, result) {
+                if (err) callback(err);
+                else db.addUser(provisionedDbItem.username, provisionedDbItem.password, function(err, result) {
+                    if (err) callback(err);
+                    else {
+                        callback(null, newdb);
+                    }
                 });
             })
         });
     });
-    return appitem;
 }
 
-function createAppItem(req, res) {
-    var defer = q.defer();
-    var fn = types[req.body.type];
-    if (fn == null) {
-        // TODO do something if no such type
-    }
-    /*
-     q.fcall(fn(req))
-     .then(save)
-     .then(defer.resolve())
-     .fail(defer.reject())
-     .end();
-     */
-    item = fn(req); // ez igy nem ok mert nem async fut
-    item.save(function (err) {
-        if (!err) {
-            defer.resolve();
-        } else {
-            defer.reject(err);
+function initDatabase(dbs, initdata, callback) {
+    var promise = q.resolve(1); // ?
+    dbs.forEach(function(d) {
+        var init = initdata[d.suffix];
+        if (init) {
+            Object.keys(init).forEach(function(collname) {
+                var initcolldata = init[collname];
+                d.db.collection(collname, function(err, collection) {
+                    if (err) callback(err);
+                    else promise = promise.then(function() { return q.ncall(collection.insert, collection, initcolldata) });
+                });
+            });
         }
     });
+    promise.then(function() { callback(null, dbs); } )
+        .fail(function(reason) { callback(reason); } );
+}
+
+function provisionImpl(appid, provisionid, initdata, newappid) {
+    var defer = q.defer();
+    q.ncall(checkProvisionId, this, appid, provisionid)
+        .then(function(result) { return q.ncall(createNewApp, this, appid, provisionid, newappid); })
+        .then(function(result) { return q.ncall(createDatabases, this, appid, newappid, '1', '1'); })
+        .then(function(dbs) { return q.ncall(initDatabase, this, dbs, initdata); })
+        .then(function(result) { console.log('resolve'); defer.resolve(result); })
+        .fail(function(reason) { console.log('reject'); defer.reject(reason); });
     return defer.promise;
 }
 
-app.post('/addappitem', function (req, res){
-    // kiszedni az appownert es hozzacsapni a kereshez
-    var promise = createAppItem(req, res);
-    q.when(promise)
-        .then(function(result) {
-            tokensrv.set(req.token, { status: 'Done', result: 'Succeeded' });
-        })
-        .fail(function(reason) {
-            tokensrv.set(req.token, { status: 'Done', result: 'Failed', reason: reason });
-        })
-    res.end(JSON.stringify(tokensrv.get(req.token)));
-});
+module.exports = {
 
-/* {
- "_id":"6150664a-31a3-4800-a832-74ad7770133a",
- "type":"QueryableDB",
- "parametername":"DatabaseSize",
- "parametervalue":"3",
- "method":"ChangeAppItem",
- "token":"bfe64c66-35be-40a4-95a2-49c5a26cbbb0"
- } */
-app.post('/changeappitem', function (req, res){
-    tokensrv.set(req.token, { status: 'Done', result: 'Succeeded' });
-    res.end(JSON.stringify(tokensrv.get(req.token)));
-});
-/* {
- "_id":"f463ee3e-b397-4dbd-9381-8f40bd0a5c86",
- "type":"ComputeUnits",
- "method":"RemoveAppItem",
- "token":"cb1eafb6-d628-440f-b5ef-f09986922318"
- }*/
-app.post('/removeappitem', function (req, res){
-    tokensrv.set(req.token, { status: 'Done', result: 'Succeeded' });
-    res.end(JSON.stringify(tokensrv.get(req.token)));
-});
+    init: function() {
+        checkProvisionId('1', '1', function(err, ok) {
+            if (err || ok.length == 0) {
+                var provisionableAppItem = new ProvisionableAppItem({ appID: '1', provisionID: '1' });
+                provisionableAppItem.save();
+            }
+        });
+        DbItem.find(function(err, dbitems) {
+            if (err || dbitems.length == 0) {
+                var dbitem = new DbItem({appID: '1', dbname: 'alma' });
+                dbitem.save();
+            }
+        });
+    },
 
-app.post('/stopappitem', function (req, res){
-    tokensrv.set(req.token, { status: 'Done', result: 'Succeeded' });
-    res.end(JSON.stringify(tokensrv.get(req.token)));
-});
+    provision: function(appid, provisionid, initdata) {
 
-app.post('/restartappitem', function (req, res){
-    tokensrv.set(req.token, { status: 'Done', result: 'Succeeded' });
-    res.end(JSON.stringify(tokensrv.get(req.token)));
-});
+        initdata = {
+            'alma': {
+                coll1: { x: 1 },
+                coll2: { y :1 }
+            },
+            'korte': {
+                coll3: { x: 1 },
+                coll4: { y :1 }
+            }
+        };
 
+        var newappid = uuid.v4();
+        var promise = provisionImpl(appid, provisionid, initdata, newappid);
+        q.when(promise)
+            .then(function(result) {
+                console.log('provisioning ok: ' + result);
+            })
+            .fail(function(reason) {
+                console.log('provisioninig failed: '+ reason);
+            })
+        return JSON.stringify({ newappid: newappid});
+    }
+}
+
+// TODO hiba eseten takaritas ?
+// TODO beszolni Balazsnak ha tortent provisionalas
