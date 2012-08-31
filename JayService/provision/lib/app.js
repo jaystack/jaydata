065b1b744410096079064a6b05bdae1a37398d83
Gabor Dolla <agdolla@gmail.com>
2012-08-31 04:13:19 -0700

    provision

--- JayService/provision/lib/app.js
+++ JayService/provision/lib/app.js
@@ -1,64 +1,26 @@
 
 var q = require('q');
 
-var mongoose = module.parent.exports.mongoose
-  , Schema = mongoose.Schema
-  , ObjectId = Schema.ObjectId;
-
 var tokensrv = module.parent.exports.tokensrv;
-
 var app = module.parent.exports.app;
-
-var AppSchema = new Schema({
-    id: { type: String, unique: true },
-    login : { type: String, unique: true, index: true },
-    password: { type: String, index: false },
-    passwordsalt: { type: String, index: false },
-    enabled: { type: Boolean, index: false }
-});
-
-var App = mongoose.model('App', AppSchema);
+var ctx = module.parent.exports.ctx;
+var provision = module.parent.exports.provision;
 
 function createApp(req, res) {
-    var defer = q.defer();
-    var app = new App({
-        id: req.body._id,
-        login: req.body.login,
-        password: req.body.password,
-        passwordsalt: req.body.passwordsalt,
-        enabled: req.body.enabled
-    });
-    app.save(function (err) {
-        if (!err) {
-console.log('create app ok')
-            defer.resolve();
-        } else {
-console.log('create app err')
-console.log(err);
-            defer.reject(err);
-        }
-    });
-    return defer.promise;
+  return ctx.findAppOwnerById(req.body.appownerid)
+  .then(function(appowner) {
+    var app = new $provision.Types.App({Id: req.body._id, AppOwnerId: req.body.appownerid, Name: req.body.name});
+    return ctx.addapp(app, appowner, 'admin', 'admin');
+   });
 }
 
-// ez tulajdonkeppen valami template kellene legyen...
-//app.post('/appowners', function (req, res){
 app.post('/addapp', function (req, res){
-    var promise = createApp(req, res);
-    q.when(promise)
-        .then(function(result) {
-console.log('set token ok '+req.token);
-            tokensrv.set(req.token, { status: 'Done', result: 'Succeeded' });
-        })
-        .fail(function(reason) {
-console.log('set token error '+req.token);
-            tokensrv.set(req.token, { status: 'Done', result: 'Failed', reason: reason });
-        })
-    res.end(JSON.stringify(tokensrv.get(req.token)));
+    module.parent.exports.tokenizedFunction(req, res, createApp);
 });
 
 app.post('/destroyapp', function (req, res){
     tokensrv.set(req.token, { status: 'Done', result: 'Succeeded' });
+	// TODO implement it
     res.end(JSON.stringify(tokensrv.get(req.token)));
 });
 
