ea887cda5925602041a5584126d10a5e3cdd4132
Peter Aron Zentai <peter.zentai@avangrade.com>
2012-09-27 07:47:15 -0700

    New: entityContext.createAdd{EntityTypeName} New: $data.createGuid Change: EntitySet.add now return the added entity

--- TypeSystem/Types/Guid.js
+++ TypeSystem/Types/Guid.js
@@ -18,6 +18,7 @@ $data.Guid.prototype.toString = function () {
 };
 
 $data.Guid.NewGuid = function () {
+    ///<description>a performant guid generator with high chance of doubling</description>
     var S4 = function () {
         return Math.floor(
             Math.random() * 0x10000 /* 65536 */
@@ -36,3 +37,49 @@ $data.Guid.NewGuid = function () {
 $data.parseGuid = function (guid) {
     return new $data.Guid(guid);
 };
+
+(function () {
+    /*!
+    Math.uuid.js (v1.4)
+    http://www.broofa.com
+    mailto:robert@broofa.com
+    
+    Copyright (c) 2010 Robert Kieffer
+    Dual licensed under the MIT and GPL licenses.
+    */
+
+    var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+
+    $data.createGuid = function (guidString) {
+        if (guidString) {
+            return new $data.Guid(guidString);
+        };
+
+        var len;
+        var chars = CHARS, uuid = [], i;
+        var radix = chars.length;
+
+        if (len) {
+            // Compact form
+            for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+        } else {
+            // rfc4122, version 4 form
+            var r;
+
+            // rfc4122 requires these characters
+            uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+            uuid[14] = '4';
+
+            // Fill in random data.  At i==19 set the high bits of clock sequence as
+            // per rfc4122, sec. 4.1.5
+            for (i = 0; i < 36; i++) {
+                if (!uuid[i]) {
+                    r = 0 | Math.random() * 16;
+                    uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+                }
+            }
+        }
+
+        return $data.parseGuid(uuid.join(''));
+    };
+})();
