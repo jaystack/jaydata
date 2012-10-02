73b6ba2c457c8f9de3b6a064daf2ecaf1f82db4f
Viktor Lázár <lazarv1982@gmail.com>
2012-10-02 11:19:27 +0200

    BatchProcessor error handling in promise

--- JayService/OData/BatchProcessor.js
+++ JayService/OData/BatchProcessor.js
@@ -89,6 +89,8 @@
                 response.statusCode = 202;
                 var res = new $data.MultiPartMixedResult(responseData.body, responseData.batchBoundary);
                 success(res);
+            }).fail(function(err){
+                error(err);
             });
         }
     },
