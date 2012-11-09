cdc5afa675ba94f224e64444d0adff0693e436e0
borzav <viktor.borza@avangrade.com>
2012-11-09 18:06:54 +0100

    ADD: $data.service accept config object in first parameter with appId, ownerId, serviceName, isSSL, port, and can be connect into JayStorm. If the ownerId is set then it will connect into open services

--- JaySvcUtil/JaySvcUtil.js
+++ JaySvcUtil/JaySvcUtil.js
@@ -584,5 +584,23 @@ $data.Class.define('$data.MetadataLoaderClass', null, null, {
 
 $data.MetadataLoader = new $data.MetadataLoaderClass();
 $data.service = function (serviceUri, cb, config) {
-    $data.MetadataLoader.load(serviceUri, cb, config);
+    var cfg;
+    if (typeof serviceUri === 'object') {
+        //appId, serviceName, ownerid, isSSL, port
+        cfg = serviceUri;
+        var protocol = cfg.isSSL || cfg.isSSL === undefined ? 'https' : 'http';
+        var port = cfg.port ? (':' + cfg.port) : '';
+
+        if (cfg.ownerId) {
+            serviceUri = protocol + '://open.jaystack.net/' + cfg.ownerId + '/' + cfg.appId + '/api/' + cfg.serviceName;
+        } else {
+            serviceUri = protocol + '://' + cfg.appId + '.jaystack.net' + port + '/' + cfg.serviceName;
+        }
+
+        cfg = $data.typeSystem.extend(cfg, config);
+    } else {
+        cfg = config;
+    }
+
+    $data.MetadataLoader.load(serviceUri, cb, cfg);
 };
