c7513fd4f07a03e6aa597ea8a97acbbc50bab48c
Viktor Lázár <lazarv1982@gmail.com>
2013-04-24 17:02:33 +0200

    URI decode in OData helper

--- TypeSystem/Types/Blob.js
+++ TypeSystem/Types/Blob.js
@@ -91,7 +91,14 @@ $data.Container.registerConverter('$data.Blob',{
             return new (typeof Buffer !== 'undefined' ? Buffer : Uint8Array)(value);
         }else if (typeof Buffer !== 'undefined' ? value instanceof Buffer : false){
             return value;
-        } else if (value.buffer) {
+        }else if (value.buffer){
+            return new (typeof Buffer !== 'undefined' ? Buffer : Uint8Array)(value);
+        }else if (typeof value == 'object' && value instanceof Object){
+            var arr = [];
+            for (var i in value){
+                arr[i] = value[i];
+            }
+            if (!arr.length) throw 0;
             return new (typeof Buffer !== 'undefined' ? Buffer : Uint8Array)(value);
         }
         throw 0;
