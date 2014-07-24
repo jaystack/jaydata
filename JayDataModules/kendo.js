ea71f1bc5e6ee6f16c066c5c1612aa5027c798e5
Ilya Lukyanov <ilya.lukyanov@gmail.com>
2014-07-24 18:44:02 +0400

    Fixed deep property navigation in kendo ds filter
    Filters like { field: 'Very.Deep.Path.To.A.Field', operator: 'contains',
    value: 'foo' } were not working

--- JayDataModules/kendo.js
+++ JayDataModules/kendo.js
@@ -483,7 +483,13 @@
                                     $data.Trace.log('unknown operator', f.operator);
                                     break;
                             }
-                            thisArg[f.field] = f.value;
+                            
+							f.field.split('.').forEach(function (fp, index, arr) {
+                                if (index < arr.length - 1)
+                                    this.ctx = (this.ctx[fp] = this.ctx[fp] || {});
+                                else
+                                    this.ctx[fp] = f.value
+                            }, { ctx: thisArg })
                         })
                         q = q.filter(filter, thisArg);
                     }
