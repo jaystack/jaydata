73ba5d9bd6371b8190d172eb2ccaef370b945c2b
PeterAronZentai <peter.zentai@avangrade.com>
2015-12-01 04:17:15 +0000

    metadata converter additions

--- src/JaySvcUtil/Metadata.js
+++ src/JaySvcUtil/Metadata.js
@@ -5,7 +5,7 @@ export class Metadata {
     
     constructor(options, metadata) {
         this.options = options
-        this.metadata - metadata
+        this.metadata = metadata
     }
     
     _getMaxValue(maxValue) {
@@ -40,17 +40,35 @@ export class Metadata {
         }
     }
 
-    createEntityTypeDefinition(entitySchema) {
+    createEntityDefinition(entitySchema) {
         var props = (entitySchema.property || []).map(this.createProperty.bind(this))
+
+        var result = {}
+        props.forEach(p => result[p.name] = p.definition)
+        
+        return result
     }
     
     createEntityType(container, entitySchema, namespace) {
-        var baseType = this.options.baseType || $data.Entity 
+        var baseType = this.options.baseType || $data.Entity
+        return baseType.extend(`${namespace}.${entitySchema.name}`, container, 
+                                this.createEntityDefinition(entitySchema))
     }
+    
+    createContextDefinition() {
+        
+    }
+    
     
     createContextType() {
         var container = $data.createContainer()
-        //var contextMembers = {}
+        this.metadata.dataServices.schema.forEach(schema => {
+            var ns = schema.namespace
+            if (schema.entityType) {
+                schema.entityType.forEach(et => this.createEntityType(container, et, ns))
+            }
+        })
+        console.log(container)
         
     }
     
@@ -58,3 +76,5 @@ export class Metadata {
 
     }
 }
+
+$data.Metadata = Metadata
--- test/unit-tests/MetadataConverter-tests.js
+++ test/unit-tests/MetadataConverter-tests.js
@@ -5,9 +5,16 @@ import {expect} from 'chai'
 describe("Metadata", function() {
     var md
     beforeEach(() => md = new Metadata({},{}))
+    
     it("should produce Metadata instances", () => {
         expect(md).to.be.instanceof(Metadata)
     })
+})
+
+describe("Metadata.properties", function() {
+    var md
+    beforeEach(() => md = new Metadata({},{}))
+    
     
     it("should create a property from a propSchema", () => {
         expect(md.createProperty({ name:"P1", type: "Edm.Int"}))
@@ -18,4 +25,53 @@ describe("Metadata", function() {
         expect(md.createProperty({ name:"P1", type: "Edm.Int", nullable: false}))
               .to.deep.equal({ name:"P1", definition: {type: "Edm.Int", nullable: false, required: true}})
     })    
+
+    it("should handle maxLength property field", () => {
+        expect(md.createProperty({ name:"P1", type: "Edm.Int", maxLength: "12"}))
+              .to.deep.equal({ name:"P1", definition: {type: "Edm.Int", maxLength:12}})
+              
+        expect(md.createProperty({ name:"P1", type: "Edm.Int", maxLength: "max"}))
+              .to.deep.equal({ name:"P1", definition: {type: "Edm.Int", maxLength:Number.MAX_VALUE}})
+    })    
+
+})
+
+describe("Metadata.createEntityType", function() {
+    var basicProps = {
+            name:"E1",
+            property: [
+                {
+                    name:"p1",
+                    type: "string"
+                },
+                {
+                    name:"p2",
+                    type:"string"
+                }
+            ]
+     }
+
+    it("should return an entity definition with matching props", function() {
+        let md = new Metadata({}, {})
+        var ed = md.createEntityDefinition(basicProps)
+        expect(ed).to.deep.equal({
+            'p1': { type:'string'},
+            'p2': { type:'string'}
+        })
+    })
+
+    it("should return an entity subclass", function() {
+        var md = new Metadata({},{})
+        var c = $data.createContainer()
+        var t = md.createEntityType(c, basicProps, "ns1")
+        expect(t.baseTypes.some(bt => bt === $data.Entity)).to.be.true
+    })
+})
+
+describe("Metadata.createContextType", function() {
+    var schema = require('../data/schema.json')
+    it("should create a context type field all entity sets", function() {
+        var md = new Metadata({},schema)
+        md.createContextType()
+    })
 })
