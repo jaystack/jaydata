df6f706cf980e8a328757313506da4ef6cae8b46
PeterAronZentai <peter.zentai@avangrade.com>
2015-12-09 02:54:20 +0000

    first working version

--- src/JaySvcUtil/Metadata.js
+++ src/JaySvcUtil/Metadata.js
@@ -1,39 +1,62 @@
 import $data, { $C, Guard, Container, Exception } from '../TypeSystem/index.js';
 
+var containsField = (obj, field, cb) => {
+    if (field in (obj || {})) {
+        cb(obj[field])
+    }
+}
+
+var parsebool = (b,d) => {
+    if ("boolean" === typeof b) {
+        return b
+    }
+    switch(b) {
+        case "true": return true
+        case "false": return false
+        default: return d
+    }
+}
 
 export class Metadata {
-    
+
     constructor(options, metadata) {
-        this.options = options
+        this.options = options || {}
         this.metadata = metadata
+        this.options.container = this.options.container || $data.createContainer()
     }
-    
+
     _getMaxValue(maxValue) {
         if ("number" === typeof maxValue) return maxValue
         if ("max" === maxValue) return Number.MAX_VALUE
         return parseInt(maxValue)
     }
-    containsField(object, field, cb) {
-        if (field in object) {
-            cb(object[field])
+
+
+    createProperty(entitySchema, propertySchema) {
+        if (!propertySchema) {
+            propertySchema = entitySchema
+            entitySchema = undefined
         }
-    }
-    
-    createProperty(propertySchema, entitySchema) {
 
         var definition = {
             type: propertySchema.type,
         }
 
-        this.containsField(propertySchema, "nullable", v => {
-            definition.nullable = v,
-            definition.required = v === false
+        containsField(propertySchema, "nullable", v => {
+            definition.nullable = parsebool(v, true),
+            definition.required = parsebool(v, true) === false
         })
 
-        this.containsField(propertySchema, "maxLength", v => {
+        containsField(propertySchema, "maxLength", v => {
             definition.maxLength = this._getMaxValue(v)
         })
-        
+
+        containsField(entitySchema, "key", keys => {
+            if (keys.some(key =>key.propertyRef.some(pr => pr.name === propertySchema.name))) {
+                definition.key = true
+            }
+        })
+
         return {
             name: propertySchema.name,
             definition
@@ -41,37 +64,74 @@ export class Metadata {
     }
 
     createEntityDefinition(entitySchema) {
-        var props = (entitySchema.property || []).map(this.createProperty.bind(this))
-
-        var result = {}
-        props.forEach(p => result[p.name] = p.definition)
-        
+        var props = (entitySchema.property || []).map(this.createProperty.bind(this, entitySchema))
+        var result = props.reduce( (p, c) => {
+            p[c.name] = c.definition
+            return p
+         }, {})
         return result
     }
-    
-    createEntityType(container, entitySchema, namespace) {
-        var baseType = this.options.baseType || $data.Entity
-        return baseType.extend(`${namespace}.${entitySchema.name}`, container, 
-                                this.createEntityDefinition(entitySchema))
+
+    createEntityType(entitySchema, namespace) {
+        let baseType = this.options.baseType || $data.Entity
+        let definition = this.createEntityDefinition(entitySchema)
+        let entityFullName = `${namespace}.${entitySchema.name}`
+        return baseType.extend(entityFullName, this.options.container, definition)
     }
-    
-    createContextDefinition() {
-        
+
+
+    createEntitySetProperty(entitySetSchema, contextSchema) {
+        var c = this.options.container
+        var t = c.classTypes[c.classNames[entitySetSchema.entityType]] // || entitySetSchema.entityType
+        var prop = {
+            name: entitySetSchema.name,
+            definition: {
+                type: $data.EntitySet,
+                elementType: t
+            }
+        }
+        return prop
     }
-    
-    
-    createContextType() {
-        var container = $data.createContainer()
+
+    indexBy(fieldName, pick) {
+        return [(p, c) => { p[c[fieldName]] = c[pick];  return p }, {}]
+    }
+
+    createContextDefinition(contextSchema, namespace) {
+        var props = (contextSchema.entitySet || []).map( es => this.createEntitySetProperty(es, contextSchema))
+
+        var result = props.reduce(...this.indexBy("name","definition"))
+        return result
+    }
+
+    createContextType(contextSchema, namespace) {
+        if (Array.isArray(contextSchema)) {
+            throw new Error("Array type is not supported here")
+        }
+        var definition = this.createContextDefinition(contextSchema, namespace)
+        var baseType = this.options.contextType || $data.EntityContext
+        var typeName = `${namespace}.${contextSchema.name}`
+        var type = baseType.extend(typeName, this.options.container, definition)
+        return type
+    }
+
+    processMetadata(createdTypes) {
+        var types = createdTypes || []
         this.metadata.dataServices.schema.forEach(schema => {
             var ns = schema.namespace
             if (schema.entityType) {
-                schema.entityType.forEach(et => this.createEntityType(container, et, ns))
+                let entityTypes = schema.entityType.map(et => this.createEntityType(et, ns))
+                types.push(...entityTypes)
+            }
+
+            if (schema.entityContainer) {
+                let contextType = this.createContextType(schema.entityContainer, ns)
+                types.push(contextType)
             }
         })
-        console.log(container)
-        
+        return types;
     }
-    
+
     convertToSourceCode(done) {
 
     }
