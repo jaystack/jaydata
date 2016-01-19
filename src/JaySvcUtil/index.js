0f708658727e359c47309c3efd042cdfdc2333ed
Lázár Viktor <lazarv1982@gmail.com>
2016-01-19 10:30:25 +0100

    Update dependencies in package.json

--- src/JaySvcUtil/index.js
+++ src/JaySvcUtil/index.js
@@ -1,6 +1,7 @@
 import $data from '../TypeSystem/index.js';
 import { DynamicMetadata } from 'jaydata-dynamic-metadata';
 
+$data.DynamicMetadata = DynamicMetadata;
 var dynamicMetadata = new DynamicMetadata($data);
 $data.service = dynamicMetadata.service.bind(dynamicMetadata);
 $data.initService = dynamicMetadata.initService.bind(dynamicMetadata);
