3dbb3b67533d03a3f121bf514e388d8538198313
borzav <viktor.borza@jaydata.org>
2016-03-29 11:57:38 +0200

    Update: npm dependencies; Remove console.log

--- src/TypeSystem/initializeJayDataClient.js
+++ src/TypeSystem/initializeJayDataClient.js
@@ -1,5 +1,5 @@
 export default function _data_handler() {
-  console.log("@@@@", this);
+  //console.log("@@@@", this);
   if (this instanceof _data_handler) {
     var type = _data_handler["implementation"].apply(this, arguments);
     return new type(arguments[1]);
