aaa89c2d37bd7c9aa9e01acad7db94f96650b1c7
Lázár Viktor <lazarv1982@gmail.com>
2016-05-02 11:06:53 +0200

    JayData compatibility scripts

---
+++ jaydata-compatibility.js
@@ -0,0 +1,8 @@
+(function(g){
+    $data.setModelContainer(g);
+    g.Container = $data.Container;
+    g.Guard = $data.Guard;
+    g.$C = $data.$C;
+    g.Exception = $data.Exception;
+    g.MemberDefinition = $data.MemberDefinition;
+})(window);
