ed1a256d2c25d6785aedf7ed56255ef8aec6e6bc
Yasser Moradi <ysmoradi@outlook.com>
2016-10-16 14:11:26 +0330

    toQueryable method now accepts type and throws an exception when no type is provied an the array itself is empty.

--- src/JayDataModules/inMemory.js
+++ src/JayDataModules/inMemory.js
@@ -2,18 +2,21 @@ import $data, { $C, Guard, Container, Exception, MemberDefinition } from 'jaydat
 
 (function ($data) {
 
-    $data.Array.prototype.toQueryable = function () {
-        if (this.length > 0) {
-            var firtsItem = this[0];
-            var type = Container.resolveType(Container.getTypeName(firtsItem));
-
-            if (!type.isAssignableTo || !type.isAssignableTo($data.Entity))
-                Guard.raise(new Exception("Type '" + Container.resolveName(type) + "' is not subclass of $data.Entity", "Not supported", type));
-
-            for (var i = 0; i < this.length; i++) {
-                Guard.requireType('array item check', this[i], type);
+    $data.Array.prototype.toQueryable = function (type) {
+        if (type == null) {
+            if(this.length > 0){
+                var firtsItem = this[0];
+                type = _core.Container.resolveType(_core.Container.getTypeName(firtsItem));
             }
+            else{
+                _core.Guard.raise(new _core.Exception("You may not call toQueryable on empty array. Either call toQueryable on non-empty array or pass entity type to toQueryable method"));
+            }
+        }
 
+        if (!type.isAssignableTo || !type.isAssignableTo($data.Entity)) _core.Guard.raise(new _core.Exception("Type '" + _core.Container.resolveName(type) + "' is not subclass of $data.Entity", "Not supported", type));
+
+        for (var i = 0; i < this.length; i++) {
+            _core.Guard.requireType('array item check', this[i], type);
         }
 
         var typeName = 'inMemoryArray_' + type.name;
